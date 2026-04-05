8aa58839506ef26a8021e71fba74efeefe088d39
hunsang jo <johunsang@gmail.com>
2026-04-05 15:06:49 +0900

    landing: v1.1.25 업데이트

--- firebase-config.js
+++ firebase-config.js
@@ -25,7 +25,7 @@ const DEFAULT_DOWNLOAD_URLS = {
 };
 
 // 현재 버전 기본값 (Firestore config 컬렉션에 저장된 값이 우선)
-const DEFAULT_VERSION = "1.1.24";
+const DEFAULT_VERSION = "1.1.25";
 
 // ============================================================
 // Firebase 초기화
